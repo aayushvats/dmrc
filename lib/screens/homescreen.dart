1f77b9c79182dfa698e167a3c692fce895c6fc54
aayushvats <44yu5hv475@gmail.com>
2024-06-09 18:25:41 +0530

    added interactions

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -24,12 +24,15 @@ class _HomePageState extends State<HomePage> {
   );
 
   final List<String> savedStations = ['AZADPUR', 'KASHMERE GATE', 'ROHINI EAST', 'KASHMERE GATE'];
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
         title: Row(
           children: [
             Container(
@@ -44,16 +47,62 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Icon(
-                  Icons.sort,
-                  color: Colors.white70,
+              child: InkWell(
+                onTap: (){
+                  _scaffoldKey.currentState?.openDrawer();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Icon(
+                    Icons.sort,
+                    color: Colors.white70,
+                  ),
                 ),
               ),
             ),
             Expanded(
               child: CurrentStation(currentStation: demoStation),
+            ),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Theme.of(context).canvasColor,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).canvasColor,
+              ),
+              child: Column(
+                children: [
+                  MetroMap(currentStation: demoStation,),
+                  Text(
+                    demoStation.name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                // Handle the home tap here
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                // Handle the settings tap here
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
@@ -81,22 +130,27 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ],
                 ),
-                child: const Padding(
+                child: Padding(
                   padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(CupertinoIcons.map, color: CupertinoColors.white, size: 18,),
-                      Text(' MAP', style: TextStyle(color: CupertinoColors.white),),
-                    ],
+                  child: InkWell(
+                    onTap: (){
+                      print("Clicked Map");
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(CupertinoIcons.map, color: CupertinoColors.white, size: 18,),
+                        Text(' MAP', style: TextStyle(color: CupertinoColors.white),),
+                      ],
+                    ),
                   ),
                 ),
               ),
               Expanded(
                 flex: 12,
                 child: Container(
-                  margin: EdgeInsets.only(top: 10, bottom: 40, left: 25, right: 25),
-                  padding: EdgeInsets.only(top: 25, bottom: 25, left: 25, right: 25),
+                  margin: EdgeInsets.only(top: 10, bottom: 35, left: 25, right: 25),
+                  padding: EdgeInsets.only(top: 25, bottom: 15, left: 25, right: 25),
                   decoration: BoxDecoration(
                     color: Colors.black,
                     borderRadius: BorderRadius.circular(30), // Adjust as needed
@@ -255,44 +309,54 @@ class _HomePageState extends State<HomePage> {
                                   )).toList()
                             ),
                           ),
-                          Container(
-                            height: 45,
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Container(
-                                    decoration: const BoxDecoration(
-                                      gradient: LinearGradient(
-                                        colors: [Colors.black, Colors.transparent],
-                                        begin: Alignment.centerRight,
-                                        end: Alignment.center,
+                          IgnorePointer(
+                            child: Container(
+                              height: 45,
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      decoration: const BoxDecoration(
+                                        gradient: LinearGradient(
+                                          colors: [Colors.black, Colors.transparent],
+                                          begin: Alignment.centerRight,
+                                          end: Alignment.center,
+                                        ),
                                       ),
                                     ),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      crossAxisAlignment: CrossAxisAlignment.end,
-                                      children: [
-                                        Container(
-                                          decoration: BoxDecoration(
-                                            color: Theme.of(context).canvasColor,
-                                            borderRadius: BorderRadius.circular(12),
-                                          ),
-                                          child: const Padding(
-                                            padding: EdgeInsets.all(8.0),
-                                            child: Icon(
-                                              CupertinoIcons.add,
-                                              color: Colors.white60,
-                                              size: 20,
-                                            ),
-                                          ),
-                                        ),
-                                      ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 41,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                InkWell(
+                                  onTap: (){
+                                    print("clicked Add Station");
+                                  },
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Theme.of(context).canvasColor,
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Icon(
+                                        CupertinoIcons.add,
+                                        color: Colors.white60,
+                                        size: 20,
+                                      ),
                                     ),
                                   ),
-                                )
+                                ),
                               ],
                             ),
-                          )
+                          ),
                         ],
                       ),
                       Divider(
@@ -303,7 +367,26 @@ class _HomePageState extends State<HomePage> {
                       Expanded(child: Placeholder()),
 
                       //BUTTON
-                      ElevatedButton(onPressed: (){}, child: Text("Search Station"))
+                      ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Theme.of(context).canvasColor),
+                        ),
+                          onPressed: (){},
+                          child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.manage_search_outlined,
+                            color: getLineColor('green'),
+                          ),
+                          Text(
+                            " SEARCH STATION",
+                            style: TextStyle(
+                              color: getLineColor('green'),
+                            ),
+                          ),
+                        ],
+                      ))
                     ],
                   ),
                 ),
